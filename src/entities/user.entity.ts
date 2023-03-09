1460d22f86d9533384991ebb437ced146c1dba53
BorisIvos <boris.ivos.b@gmail.com>
2023-03-09 13:02:57 +0100

    Add data validation to the application

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -3,9 +3,10 @@ import {
   Entity,
   Index,
   OneToMany,
-  PrimaryGeneratedColumn,
+  PrimaryGeneratedColumn
 } from "typeorm";
 import { Cart } from "./cart.entity";
+import * as Validator from 'class-validator';
 
 @Index("uq_user_email", ["email"], { unique: true })
 @Index("uq_user_phone_number", ["phoneNumber"], { unique: true })
@@ -15,39 +16,58 @@ export class User {
   userId: number;
 
   @Column({
-    type:"varchar",
+    type: "varchar",
     unique: true,
     length: 255
+  })
+  @Validator.IsNotEmpty()
+  @Validator.IsEmail({
+    allow_ip_domain: false,
+    allow_utf8_local_part: true,
+    require_tld: true,
   })
   email: string;
 
   @Column({
-    type:"varchar",
+    type: "varchar",
     name: "password_hash",
     length: 128
   })
+  @Validator.IsNotEmpty()
+  @Validator.IsHash('sha512')
   passwordHash: string;
 
-  @Column({type:"varchar", length: 64 })
+  @Column({ type: "varchar", length: 64 })
+  @Validator.IsNotEmpty()
+  @Validator.IsString()
+  @Validator.Length(2, 64)
   forename: string;
 
-  @Column({type:"varchar", length: 64})
+  @Column({ type: "varchar", length: 64 })
+  @Validator.IsNotEmpty()
+  @Validator.IsString()
+  @Validator.Length(2, 64)
   surname: string;
 
-
-
   @Column({
-    type:"varchar",
+    type: "varchar",
     name: "phone_number",
     unique: true,
     length: 24
   })
+  @Validator.IsNotEmpty()
+  @Validator.IsPhoneNumber(null)
   phoneNumber: string;
 
-  @Column({type:"text", name: "postal_address" })
+  @Column({ type: "text", name: "postal_address" })
+  @Validator.IsNotEmpty()
+  @Validator.IsString()
+  @Validator.Length(10, 512)
   postalAddress: string;
 
-  @OneToMany(() => Cart, (cart) => cart.user)
+  @OneToMany(
+    () => Cart,
+    cart => cart.user
+  )
   carts: Cart[];
-    surename: string;
 }
